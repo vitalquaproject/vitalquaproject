29d3e7bc1681606953877e82efd52008f0494805
vitalquaproject <vitalquaproject@gmail.com>
2026-07-06 17:40:56 +0200

    hamburger

--- _serve.js
+++ _serve.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs   = require('fs');
 const path = require('path');
 const port = 3000;
-const root = __dirname;
+const root = path.join(__dirname, 'public');
 
 const mime = {
   '.html': 'text/html; charset=utf-8',
@@ -19,7 +19,7 @@ const mime = {
 
 http.createServer((req, res) => {
   let urlPath = req.url.split('?')[0];
-  if (urlPath === '/') urlPath = '/concert.html';
+  if (urlPath === '/') urlPath = '/index.html';
   const filePath = path.join(root, urlPath);
   const ext = path.extname(filePath).toLowerCase();
 
@@ -32,5 +32,5 @@ http.createServer((req, res) => {
     res.end(data);
   });
 }).listen(port, '127.0.0.1', () => {
-  console.log('Vitalqua dev server running at http://localhost:' + port + '/concert.html');
+  console.log('Vitalqua dev server → http://localhost:' + port + '/');
 });
